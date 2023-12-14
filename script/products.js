4a9c16d183efe9c4416707ae1363e2684c70a158
Abishek <abiabiff1206@gmail.com>
2023-12-14 17:16:17 +0530

    till-productpage

---
+++ script/products.js
@@ -0,0 +1,60 @@
+const products=[
+    {
+        id:"car1",
+        name:"Audi A8 L 2022",
+        image:"src/images/Audi.png",
+        price:5000,
+
+    },
+    {
+        id:"car2",
+        name:"Porsche Cayenne GTS 2022",
+        image:"src/images/porsche.png",
+        price:5000,
+
+    },
+    {
+        id:"car3",
+        name:"MW X7 M50i 2022",
+        image:"src/images/MW.png",
+        price:5000,
+
+    },
+    {
+        id:"car4",
+        name:"BMW M8 Coupe 2022",
+        image:"src/images/BMW.png",
+        price:5000,
+
+    },
+    {
+        id:"car5",
+        name:"Porsche Cayenne GTS 2022",
+        image:"src/images/Porsche-Cayenne.png",
+        price:5000,
+
+    },
+    {
+        id:"car6",
+        name:"Nissan Maxima Platinum 2022",
+        image:"src/images/Nissan.png",
+        price:5000,
+
+    }
+]
+
+let productsHTML=''
+products.forEach((product)=>{
+productsHTML+=` <div class=" col-lg-4 col-md-6 col-sm-12 column "> 
+<img class="product-image" src="${product.image}">
+<p class="product-name">${product.name}</p>
+<p class="product-cost">
+  <span class="span-cost">${product.price}</span>
+  /day
+</p>
+<button class="btn btn-outline-danger rent-button">rent</button>
+
+ 
+</div>`
+})
+document.querySelector(".car-row").innerHTML=productsHTML
